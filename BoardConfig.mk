8607de62335b7300024016a0cd95c16abcb48107
root <root@ubuntu.net>
2012-08-02 01:28:22 +0800

    init device

---
+++ BoardConfig.mk
@@ -0,0 +1,129 @@
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This variable is set first, so it can be overridden
+BOARD_USES_GENERIC_AUDIO := false
+USE_CAMERA_STUB := true
+BOARD_HAVE_BLUETOOTH := false
+
+# Use the non-open-source parts, if they're present
+#-include vendor/amazon/otter/BoardConfigVendor.mk
+
+# Target information
+# Processor
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_ARMV7A := true
+TARGET_ARCH_VARIANT := armv7-a-neon
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon 
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon
+TARGET_arm_CFLAGS := -O2 -fomit-frame-pointer -fstrict-aliasing -funswitch-loops fmodulo-sched -fmodulo-sched-allow-regmoves
+TARGET_thumb_CFLAGS := -mthumb -Os -fomit-frame-pointer -fstrict-aliasing
+
+# Kernel
+
+TARGET_NO_BOOTLOADER := true
+#TARGET_NO_KERNEL := false
+TARGET_NO_RADIOIMAGE := true
+#TARGET_NO_RECOVERY := false
+
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_CMDLINE := mem=512M console=ttyO2,115200n8 vram=16M omapfb.vram=0:8M def_disp=lcd2
+TARGET_BOARD_PLATFORM := omap4
+TARGET_BOOTLOADER_BOARD_NAME := otter
+TARGET_BOARD_INFO_FILE := device/amazon/otter/board-info.txt
+TARGET_PROVIDES_INIT_RC := true
+BOARD_HAS_SDCARD_INTERNAL := true
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/platform/omap/omap_hsmmc.1/by-name/media
+BOARD_SDCARD_DEVICE_INTERNAL := /dev/block/platform/omap/omap_hsmmc.1/by-name/media
+
+# Kernel Build
+TARGET_KERNEL_CONFIG := otter_android_defconfig
+#TARGET_PREBUILT_KERNEL := device/amazon/otter/kernel
+
+KERNEL_EXTERNAL_MODULES:
+	make -C kernel/amazon/otter/external/wlan/mac80211/compat_wl12xx KERNEL_DIR=$(KERNEL_OUT) KLIB=$(KERNEL_OUT) KLIB_BUILD=$(KERNEL_OUT) ARCH=arm CROSS_COMPILE="arm-eabi-"
+	mv kernel/amazon/otter/external/wlan/mac80211/compat_wl12xx/compat/compat.ko $(KERNEL_MODULES_OUT)
+	mv kernel/amazon/otter/external/wlan/mac80211/compat_wl12xx/net/mac80211/mac80211.ko $(KERNEL_MODULES_OUT)
+	mv kernel/amazon/otter/external/wlan/mac80211/compat_wl12xx/net/wireless/cfg80211.ko $(KERNEL_MODULES_OUT)
+	mv kernel/amazon/otter/external/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx.ko $(KERNEL_MODULES_OUT)
+	mv kernel/amazon/otter/external/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_spi.ko $(KERNEL_MODULES_OUT)
+	mv kernel/amazon/otter/external/wlan/mac80211/compat_wl12xx/drivers/net/wireless/wl12xx/wl12xx_sdio.ko $(KERNEL_MODULES_OUT)
+	make -C kernel/amazon/otter/external/sgx/src/eurasia_km/eurasiacon/build/linux2/omap4430_android ARCH=arm KERNEL_CROSS_COMPILE=arm-eabi- CROSS_COMPILE=arm-eabi- KERNELSRC=$(KERNEL_OUT)/../../../../../../kernel/amazon/otter KERNELDIR=$(KERNEL_OUT) TARGET_PRODUCT="blaze_tablet" BUILD=release TARGET_SGX=540 PLATFORM_VERSION=4.0
+	mkdir $(TARGET_ROOT_OUT)/modules
+	mv $(KERNEL_OUT)/../../target/kbuild/omaplfb_sgx540_120.ko $(TARGET_ROOT_OUT)/modules/
+	mv $(KERNEL_OUT)/../../target/kbuild/pvrsrvkm_sgx540_120.ko $(TARGET_ROOT_OUT)/modules/
+
+#$(KERNEL_OUT)
+
+TARGET_KERNEL_MODULES := KERNEL_EXTERNAL_MODULES
+
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 10485760
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 536870912
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 1192230912
+BOARD_FLASH_BLOCK_SIZE := 4096
+BOARD_VOLD_MAX_PARTITIONS := 32
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun/file"
+
+# Connectivity - Wi-Fi
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wl12xx
+BOARD_WLAN_DEVICE                := wl12xx_mac80211
+BOARD_SOFTAP_DEVICE              := wl12xx_mac80211
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wl12xx_sdio.ko"
+WIFI_DRIVER_MODULE_NAME          := "wl12xx_sdio"
+WIFI_FIRMWARE_LOADER             := ""
+COMMON_GLOBAL_CFLAGS += -DUSES_TI_MAC80211
+
+# Graphics
+BOARD_EGL_CFG := device/amazon/otter/egl.cfg
+USE_OPENGL_RENDERER := true
+
+# OMAP
+OMAP_ENHANCEMENT := true
+ifdef OMAP_ENHANCEMENT
+COMMON_GLOBAL_CFLAGS += -DOMAP_ENHANCEMENT -DTARGET_OMAP4
+endif
+
+# OTA Packaging
+TARGET_PROVIDES_RELEASETOOLS := true
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/amazon/otter/releasetools/otter_ota_from_target_files
+TARGET_RELEASETOOL_IMG_FROM_TARGET_SCRIPT := device/amazon/otter/releasetools/otter_img_from_target_files
+TARGET_CUSTOM_RELEASETOOL := ./device/amazon/otter/releasetools/squisher
+
+# Misc.
+BOARD_NEEDS_CUTILS_LOG := true
+
+# CodeAurora Optimizations: msm8960: Improve performance of memmove, bcopy, and memmove_words
+TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
+TARGET_USE_KRAIT_PLD_SET := true
+TARGET_KRAIT_BIONIC_PLDOFFS := 10
+TARGET_KRAIT_BIONIC_PLDTHRESH := 10
+TARGET_KRAIT_BIONIC_BBTHRESH := 64
+TARGET_KRAIT_BIONIC_PLDSIZE := 64
+
+# Bootanimation
+TARGET_BOOTANIMATION_PRELOAD := true
+
---
+++ device.mk
@@ -0,0 +1,201 @@
+#
+# Copyright (C) 2012 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This file includes all definitions that apply to ALL otter devices, and
+# are also specific to otter devices
+#
+# Everything in this directory will become public
+
+#ifeq ($(TARGET_PREBUILT_KERNEL),)
+#LOCAL_KERNEL := device/amazon/otter/kernel
+#else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+#endif
+
+PRODUCT_AAPT_CONFIG := normal mdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+
+# Hardware HALs
+PRODUCT_PACKAGES += \
+    lights.otter \
+    audio.primary.otter\
+    audio_policy.default \
+    audio.usb.default \
+    hwcomposer.otter \
+
+PRODUCT_PACKAGES += \
+    libinvensense_mpl \
+    libaudioutils \
+    libwvm \
+    libion \
+
+# Wifi
+PRODUCT_PACKAGES += \
+    lib_driver_cmd_wl12xx \
+    dhcpcd.conf \
+    wpa_supplicant.conf \
+
+# Sound
+PRODUCT_PACKAGES += \
+    tinyplay \
+    tinymix \
+    tinycap \
+
+# Misc
+PRODUCT_PACKAGES += \
+    librs_jni \
+    com.android.future.usb.accessory \
+    libjni_pinyinime \
+    make_ext4fs \
+    setup_fs \
+    dhcpcd.conf \
+    calibrator \
+    iontest \
+    busybox \
+    su \
+    strace \
+
+# Apps
+PRODUCT_PACKAGES += \
+    Superuser \
+    Term \
+    FileManager \
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    $(call add-to-product-copy-files-if-exists,packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml)
+
+# Prebuilts /system/root
+PRODUCT_COPY_FILES += \
+    device/amazon/otter/prebuilt/root/init.rc:/root/init.rc \
+    device/amazon/otter/prebuilt/root/init.usb.rc:/root/init.usb.rc \
+    device/amazon/otter/prebuilt/root/init.omap4430.rc:/root/init.omap4430.rc \
+    device/amazon/otter/prebuilt/root/ueventd.rc:/root/ueventd.rc \
+    device/amazon/otter/prebuilt/root/ueventd.omap4430.rc:/root/ueventd.omap4430.rc \
+
+# Prebuilts /system/bin
+PRODUCT_COPY_FILES += \
+    device/amazon/otter/prebuilt/bin/fix-mac.sh:/system/bin/fix-mac.sh \
+    device/amazon/otter/prebuilt/bin/battery_log.sh:/system/bin/battery_log.sh \
+    device/amazon/otter/prebuilt/bin/idme:/system/bin/idme \
+    device/amazon/otter/prebuilt/bin/klog.sh:/system/bin/klog.sh \
+    device/amazon/otter/prebuilt/bin/temperature_log.sh:/system/bin/temperature_log.sh \
+
+# Prebuilts /system/lib
+PRODUCT_COPY_FILES += \
+    device/amazon/otter/prebuilt/lib/hw/sensors.otter.so:/system/lib/hw/sensors.otter.so \
+    device/amazon/otter/prebuilt/lib/libidme.so:/system/lib/libidme.so \
+
+# Prebuilts /system/etc
+PRODUCT_COPY_FILES += \
+    device/amazon/otter/prebuilt/etc/init.d/01fixmac:system/etc/init.d/01fixmac \
+    device/amazon/otter/prebuilt/etc/init.d/01sysctl:system/etc/init.d/01sysctl \
+    device/amazon/otter/prebuilt/etc/init.d/02oom:system/etc/init.d/02oom \
+    device/amazon/otter/prebuilt/etc/init.d/03mount:system/etc/init.d/03mount \
+    device/amazon/otter/prebuilt/etc/sysctl.conf:system/etc/sysctl.conf \
+    device/amazon/otter/prebuilt/etc/audio_policy.conf:/system/etc/audio_policy.conf \
+    device/amazon/otter/prebuilt/etc/gps.conf:/system/etc/gps.conf \
+    device/amazon/otter/prebuilt/etc/media_codecs.xml:/system/etc/media_codecs.xml \
+    device/amazon/otter/prebuilt/etc/media_profiles.xml:/system/etc/media_profiles.xml \
+    device/amazon/otter/prebuilt/etc/mountd.conf:/system/etc/mountd.conf \
+    device/amazon/otter/prebuilt/etc/vold.fstab:/system/etc/vold.fstab \
+    device/amazon/otter/prebuilt/etc/firmware/ducati-m3.bin:/system/etc/firmware/ducati-m3.bin \
+    device/amazon/otter/prebuilt/etc/firmware/ti-connectivity/wl127x-fw-4-mr.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-mr.bin \
+    device/amazon/otter/prebuilt/etc/firmware/ti-connectivity/wl127x-fw-4-plt.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-plt.bin \
+    device/amazon/otter/prebuilt/etc/firmware/ti-connectivity/wl127x-fw-4-sr.bin:system/etc/firmware/ti-connectivity/wl127x-fw-4-sr.bin \
+    device/amazon/otter/prebuilt/etc/firmware/ti-connectivity/wl1271-nvs_127x.bin:system/etc/firmware/ti-connectivity/wl1271-nvs.bin.orig \
+    device/amazon/otter/prebuilt/etc/wifi/TQS_S_2.6.ini:system/etc/wifi/TQS_S_2.6.ini \
+
+# Prebuilt /system/media
+PRODUCT_COPY_FILES += \
+    device/amazon/otter/prebuilt/media/bootanimation.zip:/system/media/bootanimation.zip \
+
+#    device/amazon/otter/prebuilt/usr/keylayout/AVRCP.kl:/system/usr/keylayout/AVRCP.kl \
+
+# Prebuilt /system/usr
+PRODUCT_COPY_FILES += \
+    device/amazon/otter/prebuilt/usr/idc/ilitek_i2c.idc:/system/usr/idc/ilitek_i2c.idc \
+    device/amazon/otter/prebuilt/usr/idc/twl6030_pwrbutton.idc:/system/usr/idc/twl6030_pwrbutton.idc \
+    device/amazon/otter/prebuilt/usr/keylayout/twl6030_pwrbutton.kl:/system/usr/keylayout/twl6030_pwrbutton.kl \
+
+#---------------------------------------
+
+# Graphics
+PRODUCT_COPY_FILES += \
+    device/amazon/otter/vendor/imgtec/lib/hw/gralloc.omap4430.so:/system/vendor/lib/hw/gralloc.omap4430.so \
+    device/amazon/otter/vendor/imgtec/lib/egl/libEGL_POWERVR_SGX540_120.so:/system/vendor/lib/egl/libEGL_POWERVR_SGX540_120.so \
+    device/amazon/otter/vendor/imgtec/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so:/system/vendor/lib/egl/libGLESv1_CM_POWERVR_SGX540_120.so \
+    device/amazon/otter/vendor/imgtec/lib/egl/libGLESv2_POWERVR_SGX540_120.so:/system/vendor/lib/egl/libGLESv2_POWERVR_SGX540_120.so \
+    device/amazon/otter/vendor/imgtec/lib/libglslcompiler_SGX540_120.so:/system/vendor/lib/libglslcompiler_SGX540_120.so \
+    device/amazon/otter/vendor/imgtec/lib/libIMGegl_SGX540_120.so:/system/vendor/lib/libIMGegl_SGX540_120.so \
+    device/amazon/otter/vendor/imgtec/lib/libpvr2d_SGX540_120.so:/system/vendor/lib/libpvr2d_SGX540_120.so \
+    device/amazon/otter/vendor/imgtec/lib/libpvrANDROID_WSEGL_SGX540_120.so:/system/vendor/lib/libpvrANDROID_WSEGL_SGX540_120.so \
+    device/amazon/otter/vendor/imgtec/lib/libPVRScopeServices_SGX540_120.so:/system/vendor/lib/libPVRScopeServices_SGX540_120.so \
+    device/amazon/otter/vendor/imgtec/lib/libsrv_init_SGX540_120.so:/system/vendor/lib/libsrv_init_SGX540_120.so \
+    device/amazon/otter/vendor/imgtec/lib/libsrv_um_SGX540_120.so:/system/vendor/lib/libsrv_um_SGX540_120.so \
+    device/amazon/otter/vendor/imgtec/lib/libusc_SGX540_120.so:/system/vendor/lib/libusc_SGX540_120.so \
+    device/amazon/otter/vendor/imgtec/bin/pvrsrvinit_SGX540_120:/system/bin/pvrsrvinit \
+    device/amazon/otter/vendor/imgtec/bin/pvrsrvctl:/system/bin/pvrsrvctl \
+    device/amazon/otter/vendor/imgtec/etc/powervr.ini:/system/etc/powervr.ini \
+
+#---------------------------------------
+
+    #persist.sys.usb.config=mass_storage,adb \
+
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.sf.lcd_density=160 \
+    dalvik.vm.heapsize=64m \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=120 \
+    ro.sf.hwrotation=270 \
+    ro.opengles.version=131072 \
+    persist.lab126.chargeprotect=1 \
+    com.ti.omap_enhancement=true \
+    omap.enhancement=true \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1 \
+    persist.sys.usb.config=adb \
+    ro.debuggable=1 \
+    ro.cwm.forbid_format = /bootloader,/dfs,/backup,/splash \
+    persist.sys.purgeable_assets=1 \
+    pm.sleep_mode=1 \
+    wifi.supplicant_scan_interval=180 \
+    windowsmgr.max_events_per_sec=200 \
+
+PRODUCT_CHARACTERISTICS := tablet,nosdcard
+
+DEVICE_PACKAGE_OVERLAYS := device/amazon/otter/overlay/aosp
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
+$(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
+
+
