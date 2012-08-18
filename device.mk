cdf4764f46054e5922e147ed5879a0c7698c7303
river <riverzhou2000@gmail.com>
2012-08-19 04:52:51 +0800

    fix path error in copying rc script

--- device.mk
+++ device.mk
@@ -91,8 +91,8 @@ PRODUCT_COPY_FILES += \
 
 # Prebuilts /system/root
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/root/init.rc:/init.rc \
-    $(LOCAL_PATH)/prebuilt/root/init.omap4430.rc:/init.omap4430.rc \
+    $(LOCAL_PATH)/prebuilt/root/init.rc:/root/init.rc \
+    $(LOCAL_PATH)/prebuilt/root/init.omap4430.rc:/root/init.omap4430.rc \
     $(LOCAL_PATH)/prebuilt/root/init.usb.rc:/root/init.usb.rc \
     $(LOCAL_PATH)/prebuilt/root/init.trace.rc:/root/init.trace.rc \
     $(LOCAL_PATH)/prebuilt/root/ueventd.rc:/root/ueventd.rc \
