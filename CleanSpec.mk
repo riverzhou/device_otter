4556e9a4864c7e17b1361a77a7f2152ed2a4edd1
river <riverzhou2000@gmail.com>
2012-08-18 20:33:07 +0800

    fix cleanSpec.mk

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -47,4 +47,6 @@
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
-$(call add-clean-step, rm -rf $(PRODUCT_OUT)/*)
+#$(call add-clean-step, rm -rf $(PRODUCT_OUT)/*)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/root/default.prop)
