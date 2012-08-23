ec417b74112b092cd80a846c07af198d36756f78
river <riverzhou2000@gmail.com>
2012-08-23 10:49:47 +0800

    clean device.mk

--- device.mk
+++ device.mk
@@ -86,20 +86,17 @@ PRODUCT_COPY_FILES += \
 
 #---------------------------------------
 
-# Prebuilts /system/root
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/root/init.omap4430.rc:/root/init.omap4430.rc \
     $(LOCAL_PATH)/prebuilt/root/init.usb.rc:/root/init.usb.rc \
     $(LOCAL_PATH)/prebuilt/root/ueventd.omap4430.rc:/root/ueventd.omap4430.rc \
 
-# Prebuilts /system/bin
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/bin/fix-mac.sh:/system/bin/fix-mac.sh \
+    $(LOCAL_PATH)/prebuilt/bin/temperature_log.sh:/system/bin/temperature_log.sh \
     $(LOCAL_PATH)/prebuilt/bin/battery_log.sh:/system/bin/battery_log.sh \
     $(LOCAL_PATH)/prebuilt/bin/klog.sh:/system/bin/klog.sh \
-    $(LOCAL_PATH)/prebuilt/bin/temperature_log.sh:/system/bin/temperature_log.sh \
 
-# Prebuilts /system/etc
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/etc/init.d/01fixmac:system/etc/init.d/01fixmac \
     $(LOCAL_PATH)/prebuilt/etc/init.d/01sysctl:system/etc/init.d/01sysctl \
@@ -112,7 +109,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/etc/vold.fstab:/system/etc/vold.fstab \
     $(LOCAL_PATH)/prebuilt/etc/wifi/TQS_S_2.6.ini:system/etc/wifi/TQS_S_2.6.ini \
 
-# Prebuilt /system/usr
+# key mapping and touchscreen files
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/usr/idc/ilitek_i2c.idc:/system/usr/idc/ilitek_i2c.idc \
     $(LOCAL_PATH)/prebuilt/usr/idc/twl6030_pwrbutton.idc:/system/usr/idc/twl6030_pwrbutton.idc \
@@ -120,7 +117,6 @@ PRODUCT_COPY_FILES += \
 
 #---------------------------------------
 
-# bootanimation
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vendor/aosp/media/bootanimation.zip:/system/media/bootanimation.zip \
 
@@ -174,28 +170,28 @@ PRODUCT_COPY_FILES += \
 ##############################################
 
 PRODUCT_PROPERTY_OVERRIDES := \
-    ro.sf.lcd_density=160 \
+    dalvik.vm.stack-trace-file=/data/tmp/traces.txt \
     dalvik.vm.heapstartsize=5m \
     dalvik.vm.heapgrowthlimit=42m \
     dalvik.vm.heapsize=128m \
-    wifi.interface=wlan0 \
-    wifi.supplicant_scan_interval=120 \
-    ro.sf.hwrotation=270 \
     ro.opengles.version=131072 \
-    persist.lab126.chargeprotect=1 \
-    com.ti.omap_enhancement=true \
-    omap.enhancement=true \
+    ro.sf.hwrotation=270 \
+    ro.sf.lcd_density=160 \
     ro.com.google.locationfeatures=1 \
     ro.com.google.networklocation=1 \
     ro.cwm.forbid_format = /bootloader,/dfs,/backup,/splash \
-    persist.sys.purgeable_assets=1 \
-    pm.sleep_mode=1 \
-    wifi.supplicant_scan_interval=180 \
-    windowsmgr.max_events_per_sec=200 \
     ro.kernel.android.bootanim=1 \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=120 \
+    wifi.supplicant_scan_interval=180 \
+    persist.sys.purgeable_assets=1 \
     persist.sys.root_access=1 \
     persist.service.adb.enable=1 \
-    dalvik.vm.stack-trace-file=/data/tmp/traces.txt \
+    windowsmgr.max_events_per_sec=200 \
+    pm.sleep_mode=1 \
+    com.ti.omap_enhancement=true \
+    omap.enhancement=true \
+    persist.lab126.chargeprotect=1 \
 
 ##############################################
 
@@ -209,7 +205,7 @@ DEVICE_PACKAGE_OVERLAYS := device/amazon/otter/overlay/aosp
 
 PRODUCT_TAGS += dalvik.gc.type-precise
 
-$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
+#$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
 #$(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
 
 
