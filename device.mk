a48e8e1f608cd176ac8a1f50331d64682e66a529
River Zhou <riverzhou2000@gmail.com>
2012-09-26 06:26:56 +0800

    Revert "temporary disable ti hwc"
    This reverts commit ae70766931d83310cd18447591c1b2d98f385a44.

--- device.mk
+++ device.mk
@@ -28,9 +28,8 @@ PRODUCT_PACKAGES += \
     audio.primary.otter\
     audio_policy.default \
     hwcomposer.default \
+    hwcomposer.otter \
     power.otter \
-
-#    hwcomposer.otter \
 
 PRODUCT_PACKAGES += \
     libinvensense_mpl \
