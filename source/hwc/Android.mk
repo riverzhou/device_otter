8c9fd9c8f28867223e629b202777a3ade58aa2d8
River Zhou <riverzhou2000@gmail.com>
2012-09-28 10:55:41 +0800

    clean hwc libs

--- source/hwc/Android.mk
+++ source/hwc/Android.mk
@@ -6,13 +6,12 @@ include $(CLEAR_VARS)
 LOCAL_PRELINK_MODULE := false
 LOCAL_ARM_MODE := arm
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/../vendor/lib/hw
-LOCAL_SHARED_LIBRARIES := liblog libEGL libcutils libutils libhardware libhardware_legacy libz libion 
+LOCAL_SHARED_LIBRARIES := liblog libEGL libcutils libutils libhardware libhardware_legacy 
 LOCAL_SRC_FILES := hwc.c 
 
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE := hwcomposer.otter
 LOCAL_CFLAGS := -DLOG_TAG=\"ti_hwc\"
-LOCAL_C_INCLUDES += external/zlib
 
 include $(BUILD_SHARED_LIBRARY)
